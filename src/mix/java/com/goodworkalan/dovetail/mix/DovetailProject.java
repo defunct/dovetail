419d9943e6533c74a684b2bbb39bc567b6aba7ec
Alan Gutierrez <alan@blogometer.com>
2010-05-04 23:14:45 -0500

    Updated Mix build for new projectlanguage.

--- src/mix/java/com/goodworkalan/dovetail/mix/DovetailProject.java
+++ src/mix/java/com/goodworkalan/dovetail/mix/DovetailProject.java
@@ -1,6 +1,5 @@
 package com.goodworkalan.dovetail.mix;
 
-import com.goodworkalan.go.go.Artifact;
 import com.goodworkalan.mix.ProjectModule;
 import com.goodworkalan.mix.builder.Builder;
 import com.goodworkalan.mix.builder.JavaProject;
@@ -10,10 +9,10 @@ public class DovetailProject extends ProjectModule {
     public void build(Builder builder) {
         builder
             .cookbook(JavaProject.class)
-                .produces(new Artifact("com.goodworkalan/dovetail/0.7"))
+                .produces("com.github.bigeasy.dovetail/dovetail/0.7")
                 .test()
                     .depends()
-                        .artifact(new Artifact("org.testng/testng/5.10/jdk15"))
+                        .include("org.testng/testng-jdk15/5.10")
                         .end()
                     .end()
                 .end()
