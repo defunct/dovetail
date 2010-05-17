6fbf97cbb3dffa8c5505b2f578dd1a0fcbfcf5ae
Alan Gutierrez <alan@blogometer.com>
2010-05-16 23:11:52 -0500

    Mix ProjectModule is now an interface.

--- src/mix/java/com/goodworkalan/dovetail/mix/DovetailProject.java
+++ src/mix/java/com/goodworkalan/dovetail/mix/DovetailProject.java
@@ -4,16 +4,24 @@ import com.goodworkalan.mix.ProjectModule;
 import com.goodworkalan.mix.builder.Builder;
 import com.goodworkalan.mix.builder.JavaProject;
 
-public class DovetailProject extends ProjectModule {
-    @Override
+/**
+ * Builds the project definition for Dovetail.
+ *
+ * @author Alan Gutierrez
+ */
+public class DovetailProject implements ProjectModule {
+    /**
+     * Build the project definition for Dovetail.
+     *
+     * @param builder
+     *          The project builder.
+     */
     public void build(Builder builder) {
         builder
             .cookbook(JavaProject.class)
                 .produces("com.github.bigeasy.dovetail/dovetail/0.7")
-                .test()
-                    .depends()
-                        .include("org.testng/testng-jdk15/5.10")
-                        .end()
+                .depends()
+                    .development("org.testng/testng-jdk15/5.10")
                     .end()
                 .end()
             .end();
