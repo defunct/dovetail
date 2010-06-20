003694d137f9024461368d71c63523294ff3b472
Alan Gutierrez <alan@blogometer.com>
2010-06-20 14:54:28 -0500

    Brace tidy of DovetailException.

--- src/main/java/com/goodworkalan/dovetail/DovetailException.java
+++ src/main/java/com/goodworkalan/dovetail/DovetailException.java
@@ -14,139 +14,132 @@ import java.util.ResourceBundle;
  * 
  * @author Alan Gutierrez
  */
-public final class DovetailException
-extends RuntimeException
-{
-    /** The serial version id. */
-    private static final long serialVersionUID = 20070821L;
-    
-    /** A list of arguments to the formatted error message. */
-    private final List<Object> arguments = new ArrayList<Object>();
-    
-    /** TODO Outgoing! The compiler has been given an empty pattern. */
-    public final static int EMPTY_PATTERN = 301;
-    
-    /** A pattern must begin with a forward slash. */
-    public final static int FIRST_FORWARD_SLASH_MISSING = 302;
-    
-    /** Unexpected empty path part. */
-    public final static int EMPTY_PATH_PART = 303;
-    
-    public final static int IDENTIFER_MISSING = 304;
+public final class DovetailException extends RuntimeException {
+	/** The serial version id. */
+	private static final long serialVersionUID = 20070821L;
 
-    /** Unexpected question mark at a given position. */
-    public final static int JAVA_IDENTIFIER_START_EXPECTED = 305;
+	/** A list of arguments to the formatted error message. */
+	private final List<Object> arguments = new ArrayList<Object>();
 
-    // TODO Document.
-    public final static int JAVA_IDENTIFIER_PART_EXPECTED = 306;
-    
-    // TODO Document.
-    public final static int UNESCAPED_FORWARD_SLASH_IN_REGULAR_EXPEESSION = 307;
+	/** TODO Outgoing! The compiler has been given an empty pattern. */
+	public final static int EMPTY_PATTERN = 301;
 
-    // TODO Document.
-    public final static int CANNOT_PARSE_REGULAR_EXPESSION = 308;
-    
-    // TODO Document.
-    public final static int INVALID_LIMIT_CHARACTER = 309;
-    
-    // TODO Document.
-    public final static int UNEXPECTED_COMMA_IN_LIMIT = 310;
+	/** A pattern must begin with a forward slash. */
+	public final static int FIRST_FORWARD_SLASH_MISSING = 302;
 
-    // TODO Document.
-    public final static int MINIMUM_LIMIT_REQUIRED = 311;
-    
-    // TODO Document.
-    public final static int CANNOT_PARSE_LIMIT_VALUE = 312;
-    
-    /** A limit specifier or path separator is epected. */
-    public final static int LIMIT_OR_SEPARATOR_EXPECTED = 313;
-    
-    // TODO Document.
-    public final static int CANNOT_SPECIFY_LIMITS_ON_EXACTLY_ONE = 314;
-    
-    // TODO Document.
-    public final static int CLOSING_BRACKED_EXPECTED = 315;
-    
-    // TODO Document.
-    public final static int PATH_SEPARATOR_EXPECTED = 316;
-    
-    // TODO Document.
-    public final static int UNESCAPED_FORWARD_SLASH_IN_FORMAT = 317;
+	/** Unexpected empty path part. */
+	public final static int EMPTY_PATH_PART = 303;
 
-    // TODO Document.
-    public final static int UNEXPECTED_END_OF_GLOB_EXPESSION = 318;
-    
-    // TODO Document.
-    public final static int MISMATCHED_IDENTIFIERS = 401;
-    
-    // TODO Document.
-    public final static int MATCH_TEST_CONSTRUCTOR_THREW_EXCEPTION = 501;
+	public final static int IDENTIFER_MISSING = 304;
 
-    // TODO Document.
-    public final static int MATCH_TEST_CONSTRUCTOR_NOT_VISIBLE = 502;
-    
-    // TODO Document.
-    public final static int FORMAT_PARAMETER_IS_NULL = 601;
-    
-    /** Have not gotten around to creating a meaninful error message. */
-    public final static int USELESS_ERROR_CODE = 0;
-    
-    /** The error code. */
-    private final int code;
+	/** Unexpected question mark at a given position. */
+	public final static int JAVA_IDENTIFIER_START_EXPECTED = 305;
 
-    /**
-     * Create an exception with the given error code.
-     * 
-     * @param code
-     *            The error code.
-     */
-    public DovetailException(int code)
-    {
-        super(Integer.toString(code));
-        this.code = code;
-    }
+	// TODO Document.
+	public final static int JAVA_IDENTIFIER_PART_EXPECTED = 306;
 
-    /**
-     * Create an exception with the given error code that wraps the given causal
-     * exception.
-     * 
-     * @param code
-     *            The error code.
-     * @param cause
-     *            The wrapped exception.
-     */
-    public DovetailException(int code, Throwable cause)
-    {
-        super(Integer.toString(code), cause);
-        this.code = code;
-    }
+	// TODO Document.
+	public final static int UNESCAPED_FORWARD_SLASH_IN_REGULAR_EXPEESSION = 307;
 
-    /**
-     * Get the error code.
-     * 
-     * @return The error code.
-     */
-    public int getCode()
-    {
-        return code;
-    }
+	// TODO Document.
+	public final static int CANNOT_PARSE_REGULAR_EXPESSION = 308;
 
-    /**
-     * Add an argument to the list of arguments to provide the formatted error
-     * message associated with the error code.
-     * 
-     * @param argument
-     *            The format argument.
-     * @return This sheaf exception for chained invocation of add.
-     */
-    public DovetailException add(Object...args)
-    {
-        for (Object argument : args)
-        {
-            arguments.add(argument);
-        }
-        return this;
-    }
+	// TODO Document.
+	public final static int INVALID_LIMIT_CHARACTER = 309;
+
+	// TODO Document.
+	public final static int UNEXPECTED_COMMA_IN_LIMIT = 310;
+
+	// TODO Document.
+	public final static int MINIMUM_LIMIT_REQUIRED = 311;
+
+	// TODO Document.
+	public final static int CANNOT_PARSE_LIMIT_VALUE = 312;
+
+	/** A limit specifier or path separator is epected. */
+	public final static int LIMIT_OR_SEPARATOR_EXPECTED = 313;
+
+	// TODO Document.
+	public final static int CANNOT_SPECIFY_LIMITS_ON_EXACTLY_ONE = 314;
+
+	// TODO Document.
+	public final static int CLOSING_BRACKED_EXPECTED = 315;
+
+	// TODO Document.
+	public final static int PATH_SEPARATOR_EXPECTED = 316;
+
+	// TODO Document.
+	public final static int UNESCAPED_FORWARD_SLASH_IN_FORMAT = 317;
+
+	// TODO Document.
+	public final static int UNEXPECTED_END_OF_GLOB_EXPESSION = 318;
+
+	// TODO Document.
+	public final static int MISMATCHED_IDENTIFIERS = 401;
+
+	// TODO Document.
+	public final static int MATCH_TEST_CONSTRUCTOR_THREW_EXCEPTION = 501;
+
+	// TODO Document.
+	public final static int MATCH_TEST_CONSTRUCTOR_NOT_VISIBLE = 502;
+
+	// TODO Document.
+	public final static int FORMAT_PARAMETER_IS_NULL = 601;
+
+	/** Have not gotten around to creating a meaninful error message. */
+	public final static int USELESS_ERROR_CODE = 0;
+
+	/** The error code. */
+	private final int code;
+
+	/**
+	 * Create an exception with the given error code.
+	 * 
+	 * @param code
+	 *            The error code.
+	 */
+	public DovetailException(int code) {
+		super(Integer.toString(code));
+		this.code = code;
+	}
+
+	/**
+	 * Create an exception with the given error code that wraps the given causal
+	 * exception.
+	 * 
+	 * @param code
+	 *            The error code.
+	 * @param cause
+	 *            The wrapped exception.
+	 */
+	public DovetailException(int code, Throwable cause) {
+		super(Integer.toString(code), cause);
+		this.code = code;
+	}
+
+	/**
+	 * Get the error code.
+	 * 
+	 * @return The error code.
+	 */
+	public int getCode() {
+		return code;
+	}
+
+	/**
+	 * Add an argument to the list of arguments to provide the formatted error
+	 * message associated with the error code.
+	 * 
+	 * @param argument
+	 *            The format argument.
+	 * @return This sheaf exception for chained invocation of add.
+	 */
+	public DovetailException add(Object... args) {
+		for (Object argument : args) {
+			arguments.add(argument);
+		}
+		return this;
+	}
 
     /**
      * Create an detail message from the error message format associated with
@@ -154,27 +147,20 @@ extends RuntimeException
      * 
      * @return The exception message.
      */
-    @Override
-    public String getMessage()
-    {
-        String key = Integer.toString(code);
+	@Override
+	public String getMessage() {
+       String key = Integer.toString(code);
         ResourceBundle exceptions = ResourceBundle.getBundle("com.goodworkalan.dovetail.exceptions");
-        String format;
-        try
-        {
-            format = exceptions.getString(key);
-        }
-        catch (MissingResourceException e)
-        {
-            return key;
-        }
-        try
-        {
-            return String.format(format, arguments.toArray());
-        }
-        catch (Throwable e)
-        {
-            throw new Error(key, e);
-        }
-    }
+		String format;
+		try {
+			format = exceptions.getString(key);
+		} catch (MissingResourceException e) {
+			return key;
+		}
+		try {
+			return String.format(format, arguments.toArray());
+		} catch (Throwable e) {
+			throw new Error(key, e);
+		}
+	}
 }
